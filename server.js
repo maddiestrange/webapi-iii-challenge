f44d944a47a2ce8c262d42e855f6c56c2d98fc20
maddiestrange <maddie.strange@gmail.com>
2019-11-07 20:40:42 -0600

    setup routing

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
+const userRouter = require("./users/userRouter")
+const postRouter = require("./posts/postRouter")
 
 const server = express();
 
 server.use(logger);
+server.use("/api/users", userRouter)
+server.use("/api/posts", postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
